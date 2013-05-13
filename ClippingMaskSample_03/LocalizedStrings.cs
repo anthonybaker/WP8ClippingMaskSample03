92b0b4f020e99190b63c84bb595d16f1bd72164d
Anthony Baker <abakerp@gmail.com>
2013-05-13 21:36:45 +0100

    Initial code commit.
    Adding Clipping Mask XAML implementation controlled by touch gestures
    using a ScrollViewer control and the ManipulationDelta event.

---
+++ ClippingMaskSample_03/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using ClippingMaskSample_03.Resources;
+
+namespace ClippingMaskSample_03
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ ClippingMaskSample_03/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿/// Author:         Anthony Baker   
+/// Date:           May 5th, 2013
+/// Class:          MainPage
+/// Description:    Controls the clipping mask animation based on the 
+///                 ScrollViewer control Manipulation Delta event.
+
+using Microsoft.Phone.Controls;
+
+namespace ClippingMaskSample_03
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnManipulationDelta(object sender, System.Windows.Input.ManipulationDeltaEventArgs e)
+        {
+            var manipulation = e.DeltaManipulation;
+            MaskTransform.Y += manipulation.Translation.Y;
+
+            // Display debug info
+            this.DebugInfo.Text = string.Format("Offset:{0}, TX:{1}, TY:{2}, SX:{3}, SY{4}, MTY:{5}",
+                this.ScrollViewer.VerticalOffset,
+                manipulation.Translation.X,
+                manipulation.Translation.Y,
+                manipulation.Scale.X,
+                manipulation.Scale.Y,
+                MaskTransform.Y);  
+        }
+    }
+}
